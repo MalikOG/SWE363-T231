42dd158b2701108c3c782ed88c0f190e3022a0d5
MalikOG <91258726+MalikOG@users.noreply.github.com>
2023-10-16 20:17:22 +0300

    JS3_Exercise1

--- Blog/JavaScript/script.js
+++ Blog/JavaScript/script.js
@@ -1,23 +1,32 @@
-images=[
-    {
-        src:"../imgs/Dead_Astronomer.jpeg",
-        alt:"Astronomer that is dead",
-    },
-    {
-        src:"../imgs/Beethoven.jpeg",
-        alt:"Image of Beethoven"
+
+class foxRetrieve{
+    constructor(jsonEvent){
+        this.image=jsonEvent.image
+        this.alt="Fox"
     }
-]
+}
+
+(async ()=> {
+    for (i=0; i < 3; i++){
+        const res= await fetch("https://randomfox.ca/floof")
+        const data= await res.json()
+        populateImages(data)
+    }
+})()
 
 
-
-for (i=0; i<images.length; i++){
+function populateImages(data){
+    fox= new foxRetrieve(data)
     let div = document.createElement("div")
     div.classList.add("watermark")
+
     let img = document.createElement("img")
-    img.src=images[i].src
-    img.alt=images[i].alt
+    img.src=fox.image
+    img.alt=fox.alt
+
     div.appendChild(img)
     document.querySelector("#images").appendChild(div)
+
 }
+
 
