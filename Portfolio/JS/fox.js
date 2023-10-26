e8d15407216519e86a59456d5383703c33566229
MalikOG <91258726+MalikOG@users.noreply.github.com>
2023-10-26 18:06:07 +0300

    JS3_EX1

---
+++ Portfolio/JS/fox.js
@@ -0,0 +1,33 @@
+
+class foxRetrieve{
+    constructor(jsonEvent){
+        this.image=jsonEvent.image
+        this.alt="Fox"
+    }
+}
+
+(async ()=> {
+    for (i=0; i < 3; i++){
+        const res= await fetch("https://randomfox.ca/floof")
+        const data= await res.json()
+        populateImages(data)
+    }
+})()
+
+
+function populateImages(data){
+    fox= new foxRetrieve(data)
+    let div = document.createElement("div")
+    div.classList.add("fox-div")
+    let img = document.createElement("img")
+    img.classList.add("fox-image")
+
+    img.src=fox.image
+    img.alt=fox.alt
+
+    div.appendChild(img)
+    document.querySelector("#images").appendChild(div)
+    
+}
+
+
