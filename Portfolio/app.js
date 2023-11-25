77567334f3af1a08629e72e27e2e050ea6b9a09d
MalikOG <91258726+MalikOG@users.noreply.github.com>
2023-11-25 23:00:23 +0300

    BE03_EX02 & BE03_EX03

---
+++ Portfolio/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use("/CSS", express.static(__dirname + '/CSS'))
+app.use("/JS", express.static(__dirname + '/JS'))
+app.use("/img", express.static(__dirname + '/img'))
+app.use("/Logos", express.static(__dirname + '/Logos'))
+
+app.get('/', (req, res) => {
+    res.status(202).sendFile(path.resolve(__dirname, 'HTML/frontpage.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.status(202).sendFile(path.resolve(__dirname, 'HTML/about.html'))
+})
+
+app.get('/contact', (req, res) => {
+    res.status(202).sendFile(path.resolve(__dirname, 'HTML/contact.html'))
+})
+
+app.get('/suggest', (req, res) => {
+    res.status(202).sendFile(path.resolve(__dirname, 'HTML/suggest.html'))
+})
+
+app.get('/playground', (req, res) => {
+    res.status(202).sendFile(path.resolve(__dirname, 'HTML/playground.html'))
+})
+
+
+app.all('*', (req, res) => {
+    res.status(404).sendFile(path.resolve(__dirname, 'HTML/notfound.html'))
+})
+
+app.listen(5001, () => {
+    console.log()
+})
