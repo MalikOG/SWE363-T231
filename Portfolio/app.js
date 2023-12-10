3cb90d69e44d43423942f57f35ce35348c42f780
MalikOG <91258726+MalikOG@users.noreply.github.com>
2023-12-11 01:29:48 +0300

    Update

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,13 +1,45 @@
 const express = require('express')
 const path = require('path')
+const fs = require("fs")
 const app = express()
 
+// First step is the authentication of the client
+app.use(authentication)
+app.use(express.static(path.join(__dirname, 'public')));
 app.use("/CSS", express.static(__dirname + '/CSS'))
 app.use("/JS", express.static(__dirname + '/JS'))
 app.use("/img", express.static(__dirname + '/img'))
 app.use("/Logos", express.static(__dirname + '/Logos'))
 app.use(express.urlencoded({extended: false}))
 
+function authentication(req, res, next) {
+    const authheader = req.headers.authorization
+    console.log(req.headers)
+ 
+    if (!authheader) {
+        let err = new Error('You are not authenticated!')
+        res.setHeader('WWW-Authenticate', 'Basic')
+        err.status = 401
+        return next(err)
+    }
+ 
+    const auth = new Buffer.from(authheader.split(' ')[1],
+        'base64').toString().split(':');
+    const user = auth[0]
+    const pass = auth[1]
+ 
+    if (user == 'admin' && pass == 'password') {
+ 
+        // If Authorized user
+        next()
+    } else {
+        let err = new Error('You are not authenticated!')
+        res.setHeader('WWW-Authenticate', 'Basic')
+        err.status = 401
+        return next(err)
+    }
+ 
+}
 
 app.get('/', (req, res) => {
     res.status(202).sendFile(path.resolve(__dirname, 'HTML/frontpage.html'))
@@ -39,7 +71,9 @@ app.get('/playground', (req, res) => {
 app.all('*', (req, res) => {
     res.status(404).sendFile(path.resolve(__dirname, 'HTML/notfound.html'))
 })
+ 
+
 
 app.listen(5001, () => {
     console.log(`Listening at port 5001...`)
-  })
+})
